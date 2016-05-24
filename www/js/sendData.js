7bd06d4b96862b8214e4eb7d8f10fbecf2ca0bb0
Florian Adonis <tanohzana@gmail.com>
2016-05-24 19:49:37 +0200

    Connexion et inscription mises en place. A faire: redirection vers page d'accueil

--- www/js/sendData.js
+++ www/js/sendData.js
@@ -1,11 +1,27 @@
+
 $('#formInscription').submit(function(){
     var postData = $(this).serialize();
     $.ajax({
         type: 'POST',
         data: postData,
         url: 'http://192.168.1.15/newUser.php',
-        complete: function(){
-            alert('User added !');
+        complete: function(data){
+            //console.log(data);
+            alert(data.responseText); //REDIRECTION
+        }
+    });
+    return false;
+});
+
+$('#formLogin').submit(function(){
+    var postData = $(this).serialize();
+    $.ajax({
+        type: 'POST',
+        data: postData,
+        url: 'http://192.168.1.15/checkUser.php',
+        complete: function(data){
+            //console.log(data);
+            alert(data.responseText); //REDIRECTION
         }
     });
     return false;
